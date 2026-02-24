ed843edd9d16fa3efd303ffc9797a3b634327572
HusMusen16 <rasmusa07@gmail.com>
2026-02-24 13:35:57 +0100

    Småfixar

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 
 @onready var button = $UI/Endless_button
+@onready var lasers = $Lasers
+@onready var plasmas = $Plasmas
 
+var plasma_scene: PackedScene = load("res://Scenes/plasma_blast.tscn")
+var laser_scene: PackedScene = load("res://Scenes/laser.tscn")
 
 func _physics_process(_delta: float) -> void:
 	pass
@@ -18,3 +22,16 @@ func _on_mothership_surge_button_pressed() -> void:
 
 func _on_spacestation_defense_button_pressed() -> void:
 	LevelManager.start_defend_spacestation_mode()
+
+
+func _on_player_laser(type: Variant, pos: Variant, dir: Variant) -> void:
+	if type == "laser":
+		var laser = laser_scene.instantiate()
+		lasers.add_child(laser)
+		laser.position = pos
+		laser.rotation_degrees = dir
+	elif type == "plasma":
+		var plasma = plasma_scene.instantiate()
+		plasmas.add_child(plasma)
+		plasma.position = pos
+		plasma.rotation_degrees = dir
